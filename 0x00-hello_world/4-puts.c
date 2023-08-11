205b665f9d4ad828a5e85489c00ca4ba324fad64
shreen-youssif <shreenyoussif22@gmail.com>
2023-08-10 18:45:12 -0700

    52

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main(){
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * return: Always 0 (success)
+ */
+int main()
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
